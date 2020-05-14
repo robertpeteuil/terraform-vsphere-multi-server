8f1479cff480300108461e3815e92feeaf3fb38a
Robert Peteuil <robert@peteuil.com>
2020-05-14 09:32:02 -0700

    adjust output

--- modules/vm-to-server/outputs.tf
+++ modules/vm-to-server/outputs.tf
@@ -1,3 +1,3 @@
 output vm_ip_addresses {
-  value = var.vsphere_server == "one" ? vsphere_virtual_machine.vm_one[0].guest_ip_addresses : vsphere_virtual_machine.vm_two[0].guest_ip_addresses
+  value = var.vsphere_server == "one" ? vsphere_virtual_machine.vm_one.guest_ip_addresses : vsphere_virtual_machine.vm_two.guest_ip_addresses
 }
